3ea7a099ea6495db7579c63bbee57f38da79c4a8
DESKTOP-3CDT6EB\HMS <ka0ka0ka.ms@gmail.com>
2022-12-20 00:54:08 +0900

    221219 26장 IO

--- GodOfJava/src/god26/io/FileSample.java
+++ GodOfJava/src/god26/io/FileSample.java
@@ -8,6 +8,9 @@ public class FileSample {
 		FileSample sample = new FileSample();
 		String pathName = "C:\\godofjava\\text";
 		sample.checkPath(pathName);
+		sample.makeDir(pathName);
+		sample.checkFileMethods(pathName);
+//		sample.deleteFile(pathName);
 
 	}
 
@@ -16,6 +19,26 @@ public class FileSample {
 		File file = new File(pathName);
 		System.out.println(pathName + " is exists? = " + file.exists());
 	}
-	//ㄴㅇㄹㄴㅇㄹㄴㅇㄹ
 
+	// 폴더 생성
+	public void makeDir(String pathName) {
+		File file = new File(pathName);
+		System.out.println("Make : " + pathName + ", result : " + file.mkdirs());
+	}
+
+	// 파일 & 경로 확인
+	public void checkFileMethods(String pathName) {
+		File file = new File(pathName);
+		System.out.println(pathName + " : directory? " + file.isDirectory());
+		System.out.println(pathName + " : file? " + file.isFile());
+		System.out.println(pathName + " : hidden? " + file.isHidden());
+		
+		//canRead() 등등 권한확인 메소드
+	}
+	
+	// 파일 삭제
+	public void deleteFile(String pathName) {
+		File file = new File(pathName);
+		System.out.println(pathName + " : delete? " + file.delete());
+	}
 }
